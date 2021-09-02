9427c189b0240590a0d8ebdafa846b83dc6919fd
Manoj Motapalukula <20951a0585@iare.ac.in>
2021-09-03 01:17:36 +0530

    new file uploaded

---
+++ letsassign1.py
@@ -0,0 +1,44 @@
+#1. add two numbers
+
+a, b = 5, 10
+print(a+b)
+
+#output--15
+
+#2.try to add integer with string and see output
+
+'''integer = 12
+string = "hello"
+print(integer+string)'''
+
+#output--TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+#3.print vaariable and string in one line with help of f string
+
+var = 10.0
+string = "apples"
+print(f'i have {var} {string}')
+
+#output--i have 10.0 apples
+
+#4. how to add space and new line in print statement
+
+----\-backslash---
+
+1. we use \n for new line.
+
+print("hello\nworld")
+
+#output
+hello
+world
+
+2. we use \t for tab or space
+
+print("hello\tworld")
+
+#output
+hello	world
+
+
+
